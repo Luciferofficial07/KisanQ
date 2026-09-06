187394ea54353e4a1b2f296f4fb649710e6d9f9a
suraj <surajshaw4450@gmail.com>
2026-09-06 20:52:47 +0530

    Connect frontend to live backend

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,5 +1,5 @@
 window.KISANQ_CONFIG = {
-  API_BASE: "http://localhost:5001",
+  API_BASE: "https://kisanq-backend-9avn.onrender.com",
   RAZORPAY_KEY: "rzp_test_TYJWFWOQ3mEj7Z",
   REMINDER_MINUTES: 30
 };
